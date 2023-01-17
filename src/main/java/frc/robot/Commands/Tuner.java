f0a72113c8515addc6382eb23f61420307cf963a
neil ma <neilm26@educbe.ca>
2023-01-16 18:52:40 -0700

    added adjustable PID

--- src/main/java/frc/robot/Commands/Tuner.java
+++ src/main/java/frc/robot/Commands/Tuner.java
@@ -35,6 +35,13 @@ public class Tuner extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+    //m_table.SetDoubleEntry("Temp", 0);
+    //SmartDashboard.putNumber("Slider: ", m_table.entry.getDouble(0));
+    // SmartDashboard.putData(m_Drivetrain.getController());
+    m_Drivetrain.getController().getController().setPID(m_table.entry.getDouble(0), 0, 0);
+    // SmartDashboard.putData(m_Drivetrain.getController());
+
+
     SmartDashboard.putNumber("calculated: ", m_Drivetrain.getController().returnCalc(m_Pigeon.getRoll(), m_Pigeon.getSetpoint()));
     SmartDashboard.putNumber("PIGEON MEASURE: ", m_Drivetrain.getController().getMeasurement());
     double right_power = Utilities.constrain(m_Drivetrain.getController().returnCalc(m_Pigeon.getRoll(), m_Pigeon.getSetpoint()),-1,1);
--- src/main/java/frc/robot/Subsystems/NetworktablesUpdated.java
+++ src/main/java/frc/robot/Subsystems/NetworktablesUpdated.java
@@ -4,35 +4,19 @@
 
 package frc.robot.Subsystems;
 
-import edu.wpi.first.networktables.DoublePublisher;
-import edu.wpi.first.networktables.DoubleSubscriber;
 import edu.wpi.first.networktables.GenericEntry;
-import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class NetworktablesUpdated extends SubsystemBase {
   /** Creates a new NetworktablesUpdated. */
-  private final NetworkTableInstance table = NetworkTableInstance.getDefault();
+  public GenericEntry entry;
 
-  private final NetworkTable dataTable = table.getTable("Drive");
-
-  public DoubleSubscriber GetDoubleEntry(String name) {
-    DoubleSubscriber get = dataTable.getDoubleTopic(name).subscribe(0);
-
-    return get;
-  }
-
-  public DoublePublisher SetDoubleEntry(String name, double defaultVal) {
-    DoublePublisher set = dataTable.getDoubleTopic(name).publish();
-
-    return set;
-  }
-
-  public GenericEntry BuildWidget() {
-    return Shuffleboard.getTab("Drive").addPersistent("Max Speed", 1).withWidget(BuiltInWidgets.kNumberSlider).getEntry();
+  public void BuildWidget() {
+    entry = Shuffleboard.getTab("Drive").addPersistent("kP", Constants.PIGEON_KP).
+          withWidget(BuiltInWidgets.kNumberSlider).getEntry();
   }
 
   public NetworktablesUpdated() {} 
